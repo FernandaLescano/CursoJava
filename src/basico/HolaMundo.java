7f26f1527112fcc615aa480d6f0a46417d710d46
FernandaLescano <fernandalescano77@gmail.com>
2020-05-18 16:43:13 -0300

    Hola Mundo modificado

--- src/basico/HolaMundo.java
+++ src/basico/HolaMundo.java
@@ -3,5 +3,6 @@ package basico;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
+		System.out.print("Agregando codigo a mi proyecto");
 	}
 }
