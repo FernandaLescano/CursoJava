f673153466cf2b431806f83783163cb3562dafbb
FernandaLescano <65564548+FernandaLescano@users.noreply.github.com>
2020-05-18 16:51:19 -0300

    Update HolaMundo.java

--- src/basico/HolaMundo.java
+++ src/basico/HolaMundo.java
@@ -3,6 +3,7 @@ package basico;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
-		System.out.print("Agregando codigo a mi proyecto");
+		System.out.println("Agregando codigo a mi proyecto");
+		System.out.println("Agregando codigo desde GitHub");
 	}
 }
